4ced6ab07109934b8305bb011965a175e4f314dc
Carlos Badenes <cbadenes@gmail.com>
2017-11-24 19:29:59 +0100

    execute docker and maven

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-echo "getting librAIry tutorial"
-git clone git@github.com:librairy/tutorial.git
-cd tutorial
+echo "getting librAIry dependencies"
 docker run -it --name tutorial -v "$PWD":/usr/src/mymaven -w /usr/src/mymaven maven:3.5.2-jdk-8-alpine mvn -Dmaven.repo.local=/usr/src/mymaven/.m2/repository clean test
