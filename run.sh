2733bbe80c5bdbfacbdf092fcd7821873041f59e
Carlos Badenes <cbadenes@gmail.com>
2017-11-24 19:03:05 +0100

    added dependencies pom

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-shopt -s expand_aliases
 echo "getting librAIry tutorial"
 git clone git@github.com:librairy/tutorial.git
 cd tutorial
-docker run -it --name tutorial -v "$PWD":/usr/src/mymaven -w /usr/src/mymaven maven:3.5.2-jdk-8-alpine mvn -Dmaven.repo.local=$PWD/.m2/repository clean test
+docker run -it --name tutorial -v "$PWD":/usr/src/mymaven -w /usr/src/mymaven maven:3.5.2-jdk-8-alpine mvn -Dmaven.repo.local="$PWD"/.m2/repository clean test
