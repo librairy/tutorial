b8bf16ab20394d35f81b98a48f4220986093f785
Carlos Badenes <cbadenes@gmail.com>
2017-11-29 09:51:13 +0100

    renamed docker container

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-echo "getting librAIry dependencies"
-docker run -it --name tutorial -v "$PWD":/usr/src/mymaven -w /usr/src/mymaven maven:3.5.2-jdk-8-alpine mvn -Dmaven.repo.local=/usr/src/mymaven/.m2/repository clean test
+echo "getting librAIry dependencies .."
+docker run -it --name test -v "$PWD":/usr/src/mymaven -w /usr/src/mymaven maven:3.5.2-jdk-8-alpine mvn -Dmaven.repo.local=/usr/src/mymaven/.m2/repository clean test
+echo "librAIry is ready for use"
